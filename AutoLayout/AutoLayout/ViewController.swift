b586b08782d2f15298e4a30a51d7def2842cbdcc
codeshiffu <78196180+codeshiffu@users.noreply.github.com>
2021-03-24 19:05:42 +0530

    AutoLayout
    1) Created Alignment constraints
    2) Leading Trailing Constraints
    3) Height Width Constraints
    4) Height width constraint related to Super View
    5) Size classes
    6) StackView

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Sarika Thakur on 2021/03/22.
+//  Copyright © 2021 Sarika Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
+
+/* Width Height
+    C       R
+    C       C
+    R       C
+    R       R
+ 
+ Max Constraints : 4
+ Min Contraints: 2
+ 
+ X, Y, W, H
+ 
+ 
+ */
